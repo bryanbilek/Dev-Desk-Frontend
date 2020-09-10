2190665cb25064c7bca3a567588d6487e94318d3
bryanbilek <bbilek35@gmail.com>
2020-09-10 18:08:57 -0500

    created a footer for the copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.footer`
+  background-color: black;
+  color: white;
+  padding: 2%;
+  margin-top: 3%;
+`;
 
 const Footer = () => {
-  return <div></div>;
+  //simple footer for the copyright
+  return (
+    <Footer>
+      <p>Copyright © 2020 DevDeskQueue. All Rights Reserved</p>
+    </Footer>
+  );
 };
 
 export default Footer;
